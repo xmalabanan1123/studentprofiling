0a7a354e7ade709922a950301a1d101389f4fbac
xmalabanan1123 <105766546+xmalabanan1123@users.noreply.github.com>
2022-05-18 19:13:27 +1100

    Add files via upload

---
+++ dbo.Class.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Class] (
+    [ID]       INT            IDENTITY (1, 1) NOT NULL,
+    [Name]     NVARCHAR (MAX) NULL,
+    [Birthday] DATETIME2 (7)  NOT NULL,
+    [Gender]   NVARCHAR (MAX) NULL,
+    [Address]  NVARCHAR (MAX) NULL,
+    [Contact]  FLOAT (53)     NOT NULL,
+    CONSTRAINT [PK_Class] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
